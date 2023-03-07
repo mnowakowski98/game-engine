fa198af7bef7e97e3b3d74581d0db717f654fc7d
Matthew Nowakowski <nowakowskim98@gmail.com>
2023-03-07 16:56:52 -0500

    Update presentation

--- index.ts
+++ index.ts
@@ -2,8 +2,14 @@ import { startRendering } from './src/render-loop'
 
 addEventListener('load', () => {
     const canvas = document.createElement('canvas')
-    canvas.width = window.innerWidth
-    canvas.height = window.innerWidth
+    const canvasPadding = 100
+    canvas.width = innerWidth - canvasPadding
+    canvas.height = innerHeight - canvasPadding
+    canvas.style.margin = `${canvasPadding / 2}px`
+    canvas.style.border = '1px solid black'
+
+    document.body.style.margin = '0'
+    document.body.style.backgroundColor = '#a3a3a3'
     document.body.appendChild(canvas)
     
     const context = canvas.getContext('2d')
--- src/render-loop.ts
+++ src/render-loop.ts
@@ -3,11 +3,7 @@ let requestId: number
 function renderFrame(context: CanvasRenderingContext2D) {
     const canvasWidth = context.canvas.width
     const canvasHeight = context.canvas.height
-
     context.clearRect(0, 0, canvasWidth, canvasHeight)
-
-    context.lineWidth = 10;
-    context.strokeRect(0, 0, canvasWidth, canvasHeight)
 
     requestId = requestAnimationFrame(() => renderFrame(context))
 }
