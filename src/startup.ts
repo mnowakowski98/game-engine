c4705a5441d67afb8e63eeaab8320cac24adcaa1
Matthew Nowakowski <nowakowskim98@gmail.com>
2023-03-15 15:19:05 -0400

    Added placeholder menu scene

--- src/startup.ts
+++ src/startup.ts
@@ -2,11 +2,12 @@ import { startGame } from './scenes/game'
 import { registerInputs } from './inputs'
 import { showGameOver } from './scenes/game-over'
 import Scene, { startScene } from './engine/scene/scene'
+import { startMenu } from './scenes/menu'
 
 export function start(context: CanvasRenderingContext2D) {
     const canvas = context.canvas
     registerInputs(canvas)
-
+    
     const game: Scene = {
         endSceneEventType: 'game-end',
         init: () => startGame(canvas.width, canvas.height),
@@ -19,5 +20,11 @@ export function start(context: CanvasRenderingContext2D) {
         onSceneEnd: () => startScene(game, context)
     }
 
-    startScene(game, context)
+    const menu: Scene = {
+        endSceneEventType: 'menu-newgame',
+        init: () => startMenu(canvas.width, canvas.height),
+        onSceneEnd: () => startScene(game, context)
+    }
+
+    startScene(menu, context)
 }
