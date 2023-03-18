1908a18d972f2eab5ad16223093e7bb5a06f0579
Matthew Nowakowski <nowakowskim98@gmail.com>
2023-03-18 18:32:42 -0400

    Made canvas border easier to identify

--- index.ts
+++ index.ts
@@ -4,21 +4,14 @@ addEventListener('load', () => {
     document.body.style.margin = '0'
     document.body.style.backgroundColor = '#a3a3a3'
 
-    const container = document.createElement('div')
-    const containerPadding = 100
-    container.style.width = `${innerWidth - containerPadding}px`
-    container.style.height = `${innerHeight - containerPadding}px`
-    container.style.margin = `${containerPadding / 2}px`
-    container.style.border = '1px solid blue'
-    document.body.appendChild(container)
-
     const canvas = document.createElement('canvas')
     
-    canvas.width = container.clientWidth
-    canvas.height = container.clientHeight
-    canvas.style.border = '1px solid black'
+    canvas.style.boxSizing = 'border-box'
+    canvas.width = innerWidth - 6
+    canvas.height = innerHeight - 6
+    canvas.style.border = '3px dashed black'
 
-    container.appendChild(canvas)
+    document.body.appendChild(canvas)
     
     const context = canvas.getContext('2d')
     if(context == null) {
