a72af3d08dde15ad74ddb1df3afaf6012cbbab19
Matthew Nowakowski <nowakowskim98@gmail.com>
2023-05-11 15:16:52 -0400

    Technicality

--- src/project/asteroids/composer.ts
+++ src/project/asteroids/composer.ts
@@ -1,9 +1,8 @@
-import { Context } from '../../foundation/engine/rendering/canvas'
 import { startScene } from '../../feature/scene/scene'
 import { start } from './scenes/game'
 
 
-export default function startGame(context: Context) {
+export default function startGame(context: CanvasRenderingContext2D) {
     const { width, height } = context.canvas
     const gameScene = start(width, height)
     startScene(context, gameScene)
