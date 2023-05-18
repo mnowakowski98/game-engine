4956b3e8231822128d1fca3a7f0bc10ad7cbfcdf
Matthew Nowakowski <nowakowskim98@gmail.com>
2023-05-18 09:55:48 -0400

    Made rotation use the much betterer atan2 function

--- src/foundation/engine/space/rotation.ts
+++ src/foundation/engine/space/rotation.ts
@@ -1,11 +1,14 @@
 import Coordinate from './coordinates'
 
-export interface Rotatable {
-    rotation: number
-}
-
 export type Radians = number
 export type Degrees = number
+
+export interface Rotatable {
+    rotation: Radians
+}
+
+export const quarter = Math.PI / 2
+export const half = Math.PI
 
 export function deg2rad(degrees: Degrees): Radians {
     return (degrees * Math.PI) / 180
@@ -17,20 +20,5 @@ export function rad2deg(radians: Radians): Degrees {
 
 export function rotationToPosition(position: Coordinate): Radians {
     const { x: A, y: O } = position
-    let rotation = Math.atan(O / A)
-
-    // Detect quadrant and correct rotation
-    // I have no idea why these are offset and trying to figure out trig hurts
-    // Probably cause canvas uses flipped y values
-    if (A <= 0 && O <= 0) rotation -= deg2rad(90) // Q2
-    if (A >= 0 && O <= 0) rotation += deg2rad(90) // Q1
-    if (A >= 0 && O >= 0) rotation += deg2rad(90) // Q4
-    if (A <= 0 && O >= 0) rotation -= deg2rad(90) // Q3
-
-    // Corrections for 0 cases
-    if (A == 0) rotation += deg2rad(90)
-    if (O == 0) rotation -= deg2rad(90)
-    if (O == 0 && A <= 0) rotation += deg2rad(180)
-
-    return rotation
+    return Math.atan2(O, A)
 }
