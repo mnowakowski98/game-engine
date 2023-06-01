523fa962d0810f3a780b5d488a9cbafeabe868b5
Matthew Nowakowski <nowakowskim98@gmail.com>
2023-05-31 21:26:35 -0400

    Added some mouse data

--- src/feature/input/input.ts
+++ src/feature/input/input.ts
@@ -1,4 +1,5 @@
 import Unique from '../../foundation/base-types/unique'
+import { originDistance } from '../../foundation/engine/space/distance'
 
 type PressTypes = 'press' | 'press-release' | 'press-hold' | 'press-hold-release'
 type HoldTypes = 'hold' | 'press-hold' | 'hold-release' | 'press-hold-release'
@@ -12,11 +13,11 @@ type CommandTypes = ButtonEventTypes | AxisEventsTypes
 export interface Command {
     type: CommandTypes
     code: string
-    action: () => void
+    action: (value: boolean | number) => void
 }
 
 export interface InputContext extends Unique {
-    commands: Command[]
+    commands?: Command[]
 }
 
 interface ContextState {
@@ -34,35 +35,50 @@ export function startInputContext(context: InputContext): ContextState {
         
         buttonStates.set(event.code, true)
 
+        if (!context.commands) return
         const executableCommands = context.commands.filter(command =>
             (command.type.includes('press') || command.type.includes('hold')) && command.code === event.code)
-        executableCommands.forEach(command => command.action())
+        executableCommands.forEach(command => command.action(true))
     }
 
     const onKeyUp = (event: KeyboardEvent) => {
         buttonStates.set(event.code, false)
 
+        if (!context.commands) return
         const executableCommands = context.commands.filter(command =>
             (command.type.includes('release') || command.type.includes('hold')) && command.code === event.code)
-        executableCommands.forEach(command => command.action())
+        executableCommands.forEach(command => command.action(false))
     }
 
     const onMouseMove = (event: MouseEvent) => {
         axisStates.set('mouseX', event.movementX)
         axisStates.set('mouseY', event.movementY)
+
+        if (!context.commands) return
+        const executableCommands = context.commands.filter(command => command.type === 'change' && command.code === 'mouse')
+        executableCommands.forEach(command => command.action(originDistance(event.movementX, event.movementY)))
+
+        setTimeout(() => {
+            axisStates.set('mouseX', 0)
+            axisStates.set('mouseY', 0)
+        }, 10)
     }
 
     addEventListener('keydown', onKeyDown)
     addEventListener('keyup', onKeyUp)
     addEventListener('mousemove', onMouseMove)
 
-    return {
+    const state: ContextState = {
         buttonStates: buttonStates,
         axisStates: axisStates,
         stop: () => {
             removeEventListener('keydown', onKeyDown)
             removeEventListener('keyup', onKeyUp)
             removeEventListener('mousemove', onMouseMove)
+            state.buttonStates = new Map()
+            state.axisStates = new Map()
         }
     }
+
+    return state
 }
