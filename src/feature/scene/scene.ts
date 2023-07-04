954ea64c7dcc432b9ad07701b15722c23cbf0b11
Matthew Nowakowski <nowakowskim98@gmail.com>
2023-07-04 14:48:01 -0400

    Exported types used in scene feature to projects

--- src/feature/scene/scene.ts
+++ src/feature/scene/scene.ts
@@ -6,8 +6,6 @@ import { startRenderLoop } from '../../foundation/engine/rendering/render-loop'
 import { startUpdateLoop } from '../../foundation/engine/update/update-loop'
 import Unique from '../../foundation/base-types/unique'
 
-export { Actor } from '../../foundation/engine/rendering/world'
-
 export default interface Scene extends Unique {
     cameras?: () => ({camera: Camera, position: Coordinate})[]
     world?: () => World
@@ -22,5 +20,3 @@ export function startScene(canvas: Canvas, scene: Scene): () => void {
         stopUpdateLoop()
     }
 }
-
-export { Camera, World, Coordinate, Unique }
